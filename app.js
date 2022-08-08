26ff3b9f34dce811281dd1aad206035b10cb1745
hansdav <hdavidson21@gmail.com>
2022-08-08 12:14:16 +0200

    four math functions

---
+++ app.js
@@ -0,0 +1,27 @@
+function Add(a,b) {
+let sum = a + b;
+return sum
+}
+
+// console.log(Add(1,2))
+
+function Subtract(a,b) {
+    let sum = a - b;
+    return sum
+}
+
+// console.log(Subtract(5,1))
+
+function Multiply(a,b) {
+    let sum = a * b;
+    return sum
+}
+
+// console.log(Multiply(3,3))
+
+function Divide(a,b) {
+    let sum = a/b;
+    return sum
+}
+
+// console.log(Divide(10,2))
